81c781030fe995db9e4d96103426f920ad1beaa4
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-02-04 21:58:35 +0530

    added not found page

---
+++ app/+not-found.tsx
@@ -0,0 +1,25 @@
+import { View, Text } from "react-native";
+import { Button } from "react-native-paper";
+import { useRouter } from "expo-router";
+
+export default function NotFoundScreen() {
+  const router = useRouter();
+
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-100 dark:bg-gray-900">
+      <Text className="text-2xl font-bold text-gray-800 dark:text-gray-200">
+        404 - Page Not Found
+      </Text>
+      <Text className="text-gray-600 dark:text-gray-400 mt-2 text-center px-6">
+        The page you are looking for does not exist or has been moved.
+      </Text>
+      <Button
+        mode="contained"
+        // onPress={() => router.replace("/(root)/(auth)/login")}
+        className="mt-6 bg-blue-600 dark:bg-blue-400"
+      >
+        Go Home
+      </Button>
+    </View>
+  );
+}
