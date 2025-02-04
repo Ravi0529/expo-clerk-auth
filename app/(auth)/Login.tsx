6770cb7ea5ca838ec724b8f573854d658e36c754
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-02-05 02:50:22 +0530

    clerk auth successful for login

--- app/(auth)/Login.tsx
+++ app/(auth)/Login.tsx
@@ -1,21 +1,41 @@
-import { View, Text } from "react-native";
-import React, { useState } from "react";
+import { View, Text, Alert } from "react-native";
+import React, { useCallback, useState } from "react";
 import CustomButton from "@/components/CustomButton";
 import { Link, router } from "expo-router";
 import InputField from "@/components/InputField";
 import { SafeAreaView } from "react-native-safe-area-context";
 import OAuth from "@/components/OAuth";
+import { useSignIn } from "@clerk/clerk-expo";
 
 const Signup = () => {
+  const { signIn, setActive, isLoaded } = useSignIn();
 
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
 
-  const onLoginPress = () => {
-    // code
-  };
+  const onSignInPress = useCallback(async () => {
+    if (!isLoaded) return;
+
+    try {
+      const signInAttempt = await signIn.create({
+        identifier: form.email,
+        password: form.password,
+      });
+
+      if (signInAttempt.status === "complete") {
+        await setActive({ session: signInAttempt.createdSessionId });
+        router.replace("/(root)/(tabs)/Home");
+      } else {
+        console.log(JSON.stringify(signInAttempt, null, 2));
+        Alert.alert("Error", "Log in failed. Please try again.");
+      }
+    } catch (err: any) {
+      console.log(JSON.stringify(err, null, 2));
+      Alert.alert("Error", err.errors[0].longMessage);
+    }
+  }, [isLoaded, form]);
 
   return (
     <SafeAreaView className="flex-1 bg-white justify-center">
@@ -41,7 +61,7 @@ const Signup = () => {
           />
           <CustomButton
             title="Login"
-            onPress={onLoginPress}
+            onPress={onSignInPress}
             className="bg-blue-600 text-white p-2"
           />
 
