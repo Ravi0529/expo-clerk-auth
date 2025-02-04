9eee8b4582d8cee9acc9f369ebaa88a4b2bc12fe
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-02-05 01:04:49 +0530

    clerk auth init

--- app/(root)/(tabs)/Home.tsx
+++ app/(root)/(tabs)/Home.tsx
@@ -1,10 +1,23 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
+import { Link } from 'expo-router'
+import { Text, View } from 'react-native'
 
-export default function Home() {
+export default function Page() {
+  const { user } = useUser()
+
   return (
-    <SafeAreaView className="flex-1 justify-center items-center">
-      <Text>Home screen</Text>
-    </SafeAreaView>
-  );
+    <View>
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+      </SignedIn>
+      <SignedOut>
+        <Link href="/(auth)/Login">
+          <Text>Sign in</Text>
+        </Link>
+        <Link href="/(auth)/Signup">
+          <Text>Sign up</Text>
+        </Link>
+      </SignedOut>
+    </View>
+  )
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,26 @@
 import { Stack } from "expo-router";
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
+import { tokenCache } from "@/lib/cache";
 
 export default function RootLayout() {
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+  if (!publishableKey) {
+    throw new Error(
+      "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+    );
+  }
+
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      <Stack.Screen name="(root)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" options={{ headerShown: false }} />
-    </Stack>
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <ClerkLoaded>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(root)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" options={{ headerShown: false }} />
+        </Stack>
+      </ClerkLoaded>
+    </ClerkProvider>
   );
 }
